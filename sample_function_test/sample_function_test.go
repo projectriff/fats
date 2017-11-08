d3bb705b465a221b2b98f37fbc97f0a994e52e9d
Felix Reyn <freyn@pivotal.io>
2017-11-08 12:42:49 -0500

    End-to-end test, with a kafka message consumer - first pass
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- sample_function_test/sample_function_test.go
+++ sample_function_test/sample_function_test.go
@@ -2,16 +2,37 @@ package sample_function_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/pivotal-cf/pfs-system-test/util"
+	"path"
+	"github.com/onsi/gomega"
 )
 
 var _ = Describe("SampleFunctionTest", func() {
 	Describe("SampleFunctionTest", func() {
 
-		Context("the sun shines", func() {
+		Context("deploy sample uppercase java function", func() {
 
-			It("all is well", func() {
-				Expect(true).To(BeTrue())
+			It("builds and deploys", func() {
+				functionDir := path.Join(util.TEST_CONFIG.BaseDir, "sample_functions", "java", "uppercase")
+
+				functionName := util.RandStringShort()
+				inputTopicName := util.RandStringShort()
+				outputTopicName := util.RandStringShort()
+				imageName := util.TEST_CONFIG.DockerOrg + "/" + functionName
+
+				util.MvnCleanPackage(functionDir)
+				util.CLI(functionDir, "-n", functionName, "build")
+				util.DockerTagAndPush(functionName, imageName)
+				util.CLI("/", "topics-create", "-t", inputTopicName, "-ns", util.TEST_CONFIG.Namespace)
+				util.CLI("/", "topics-create", "-t", outputTopicName, "-ns", util.TEST_CONFIG.Namespace)
+				util.CLI("/", "-n", functionName, "push", "-i", inputTopicName, "-o", outputTopicName, "-ns", util.TEST_CONFIG.Namespace, "-m", imageName)
+				util.SendMessageToGateway(inputTopicName, "hello")
+
+				outputMessage := util.KubectlFromKafkaPod(outputTopicName)
+				gomega.Expect(outputMessage).To(gomega.MatchRegexp(`(?s:
+.* contentType"application/octet-stream"spanId.*
+spanTraceId.*spanParentSpanId.*
+spanSampled.*spanName"` + inputTopicName + `:output.*HELLO.*)`))
 			})
 		})
 	})
---
+++ util/test_util.go
@@ -0,0 +1,138 @@
+package util
+
+import (
+	"os"
+	"math/rand"
+	"os/exec"
+	"github.com/onsi/ginkgo"
+	"bytes"
+	"strings"
+	"time"
+	"strconv"
+)
+
+var TEST_CONFIG = InitSystemTestConfig()
+
+type SystemTestConfig struct {
+	CLIPath          string
+	Namespace        string
+	KafkaPodName     string
+	HTTPGatewayURL   string
+	DockerOrg        string
+	DockerUsername   string
+	DockerPassword   string
+	BaseDir          string
+	MessageRTTimeout int
+}
+
+func InitSystemTestConfig() SystemTestConfig {
+
+	return SystemTestConfig{
+		CLIPath:          ensureEnv("SYS_TEST_CLI_PATH"),
+		BaseDir:          ensureEnv("SYS_TEST_BASE_DIR"),
+		Namespace:        ensureEnv("SYS_TEST_NS"),
+		KafkaPodName:     ensureEnv("SYS_TEST_KAFKA_POD_NAME"),
+		HTTPGatewayURL:   ensureEnv("SYS_TEST_HTTP_GW_URL"),
+		DockerOrg:        ensureEnv("SYS_TEST_DOCKER_ORG"),
+		DockerUsername:   ensureEnv("SYS_TEST_DOCKER_USERNAME"),
+		DockerPassword:   ensureEnv("SYS_TEST_DOCKER_PASSWORD"),
+		MessageRTTimeout: ensureEnvInt("SYS_TEST_MSG_RT_TIMEOUT_SEC"),
+	}
+}
+
+func ensureEnv(varName string) (string) {
+	varValue := os.Getenv(varName)
+	if varValue == "" {
+		panic("Expected [" + varName + "] environment variable to be set")
+	}
+	return varValue
+}
+func ensureEnvInt(varName string) (int) {
+	varValue := ensureEnv(varName)
+	intValue, err := strconv.Atoi(varValue)
+	if err != nil {
+		panic("Couldn't parse [" + varName + "/" + varValue + "] as integer")
+	}
+	return intValue
+}
+
+func CLI(directory string, args ...string) {
+	runSafely("FaaS CLI", directory, TEST_CONFIG.CLIPath, args...)
+}
+
+func SendMessageToGateway(topic string, message string) {
+	runSafely("Curl", "/", "curl", "-H'Content-Type:text/plain'", "-d", message, TEST_CONFIG.HTTPGatewayURL+"/messages/"+topic)
+}
+func KubectlFromKafkaPod(topic string) string {
+
+	outBuffer := bytes.NewBufferString("")
+	cmd := exec.Command("kubectl", "-n", TEST_CONFIG.Namespace, "exec", TEST_CONFIG.KafkaPodName, "--", "/opt/kafka/bin/kafka-console-consumer.sh", "--bootstrap-server", "localhost:9092", "--topic", topic, "--from-beginning", "--max-messages", "1")
+	cmd.Stdout = outBuffer
+	cmd.Stderr = ginkgo.GinkgoWriter
+
+	if err := cmd.Start(); err != nil {
+		panic("Kubectl Kafka start failed")
+	}
+	timer := time.AfterFunc(time.Duration(TEST_CONFIG.MessageRTTimeout)*time.Second, func() {
+		cmd.Process.Kill()
+		panic("Kubectl Kafka timed out")
+
+	})
+	err := cmd.Wait()
+	timer.Stop()
+
+	if err != nil {
+		panic("Kubectl Kafka failed")
+	}
+	return outBuffer.String()
+}
+
+func DockerTagAndPush(functionName string, imageName string) {
+
+	outBuffer := bytes.NewBufferString("")
+	dockerCmd := exec.Command("docker", "images", "-f", "reference="+functionName, "--format", "{{.ID}}")
+	dockerCmd.Stdout = outBuffer
+	dockerCmd.Stderr = ginkgo.GinkgoWriter
+	err := dockerCmd.Run()
+	if err != nil {
+		panic("Docker image list failed")
+	}
+
+	runSafely("Docker Tag", "/", "docker", "tag", strings.TrimSpace(outBuffer.String()), imageName)
+	runSafely("Docker Login", "/", "docker", "login", "-u", TEST_CONFIG.DockerUsername, "-p", TEST_CONFIG.DockerPassword)
+	runSafely("Docker Push", "/", "docker", "push", imageName)
+}
+
+func MvnCleanPackage(directory string) {
+	runSafely("Maven Build", directory, "mvn", "clean", "package")
+}
+
+func runSafely(description string, directory string, command string, args ...string) {
+	cmd := exec.Command(command, args...)
+	cmd.Dir = directory
+	cmd.Stdout = ginkgo.GinkgoWriter
+	cmd.Stderr = ginkgo.GinkgoWriter
+	err := cmd.Run()
+	if err != nil {
+		panic(description + " failed in directory " + directory)
+	}
+}
+
+const letters = "abcdefghijklmnopqrstuvwxyz"
+
+func RandStringShort() string {
+	return randString(8)
+}
+
+func RandStringLong() string {
+	return randString(24)
+}
+
+func randString(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
