ed851d4005f2bd84d9326d75e8a4c18b62380032
Felix Reyn <freyn@pivotal.io>
2018-01-03 14:58:41 -0500

    Add python and shell tests

--- sample_function_test/sample_function_test.go
+++ sample_function_test/sample_function_test.go
@@ -75,5 +75,75 @@ var _ = Describe("SampleFunctionTest", func() {
 				util.DeleteFile(workloadFileTarget)
 			})
 		})
+
+		Context("deploy sample uppercase python function", func() {
+
+			It("builds and deploys", func() {
+				functionDir := path.Join(util.TEST_CONFIG.BaseDir, "samples", "python", "uppercase")
+
+				functionName := util.RandStringShort()
+				inputTopicName := util.RandStringShort()
+				imageName := util.TEST_CONFIG.DockerOrg + "/" + functionName
+				workloadFileSource := path.Join(functionDir, "uppercase.yaml")
+				workloadFileTarget := path.Join(functionDir, functionName+".yaml")
+
+				customDockerize := "dockerize" + functionName
+				util.CopyAndReplace(path.Join(functionDir, "dockerize"), path.Join(functionDir, customDockerize), "docker build", " # docker build")
+				util.Run(functionDir, "bash", path.Join(functionDir, customDockerize))
+
+				util.DockerBuild(functionDir, imageName)
+				util.DockerPush(imageName)
+				util.CopyAndReplace(workloadFileSource, workloadFileTarget, "name: uppercase", "name: "+functionName)
+				util.CopyAndReplace(workloadFileTarget, workloadFileTarget, "name: greetings", "name: "+inputTopicName)
+				util.CopyAndReplace(workloadFileTarget, workloadFileTarget, "input: greetings", "input: "+inputTopicName)
+				util.CopyAndReplace(workloadFileTarget, workloadFileTarget, "image: projectriff/grpc-py:.*", "image: "+imageName)
+
+				util.KubectlApply(workloadFileTarget, util.TEST_CONFIG.Namespace)
+				reply := util.SendRequestToGateway(inputTopicName, "foobar")
+
+				gomega.Expect(reply).To(gomega.MatchRegexp(`(?s:.*FOOBAR.*)`))
+
+				util.KubectlDelete(workloadFileTarget, util.TEST_CONFIG.Namespace)
+				util.DeleteFile(workloadFileTarget)
+				util.DeleteFile(customDockerize)
+			})
+		})
+
+		Context("deploy sample echo shell function", func() {
+
+			It("builds and deploys", func() {
+				functionDir := path.Join(util.TEST_CONFIG.BaseDir, "samples", "shell", "echo")
+
+				functionName := util.RandStringShort()
+				inputTopicName := util.RandStringShort()
+				imageName := util.TEST_CONFIG.DockerOrg + "/" + functionName
+				topicWorkloadFileSource := path.Join(functionDir, "greetings-topic.yaml")
+				topicWorkloadFileTarget := path.Join(functionDir, functionName+"-topic.yaml")
+				fnWorkloadFileSource := path.Join(functionDir, "echo-function.yaml")
+				fnWorkloadFileTarget := path.Join(functionDir, functionName+".yaml")
+
+				util.CopyAndReplace(path.Join(functionDir, "Dockerfile"), path.Join(functionDir, "Dockerfile"), "shell-function-invoker:.*", "shell-function-invoker:"+util.TEST_CONFIG.ShellInvokerVersion)
+
+				util.DockerBuild(functionDir, imageName)
+				util.DockerPush(imageName)
+
+				util.CopyAndReplace(topicWorkloadFileSource, topicWorkloadFileTarget, "name: greetings", "name: "+inputTopicName)
+
+				util.CopyAndReplace(fnWorkloadFileSource, fnWorkloadFileTarget, "name: echo", "name: "+functionName)
+				util.CopyAndReplace(fnWorkloadFileTarget, fnWorkloadFileTarget, "input: greetings", "input: "+inputTopicName)
+				util.CopyAndReplace(fnWorkloadFileTarget, fnWorkloadFileTarget, "image: projectriff/echo:.*", "image: "+imageName)
+
+				util.KubectlApply(topicWorkloadFileTarget, util.TEST_CONFIG.Namespace)
+				util.KubectlApply(fnWorkloadFileTarget, util.TEST_CONFIG.Namespace)
+				reply := util.SendRequestToGateway(inputTopicName, "fooo")
+
+				gomega.Expect(reply).To(gomega.MatchRegexp(`(?s:.*fooo.*)`))
+
+				util.KubectlDelete(fnWorkloadFileTarget, util.TEST_CONFIG.Namespace)
+				util.KubectlDelete(topicWorkloadFileTarget, util.TEST_CONFIG.Namespace)
+				util.DeleteFile(fnWorkloadFileTarget)
+				util.DeleteFile(topicWorkloadFileTarget)
+			})
+		})
 	})
 })
--- util/test_util.go
+++ util/test_util.go
@@ -6,40 +6,44 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
-	"regexp"
 )
 
 var TEST_CONFIG = InitSystemTestConfig()
 
 type SystemTestConfig struct {
-	JavaInvokerVersion string
-	NodeInvokerVersion string
-	Namespace          string
-	KafkaPodName       string
-	HTTPGatewayURL     string
-	DockerOrg          string
-	DockerUsername     string
-	DockerPassword     string
-	BaseDir            string
-	MessageRTTimeout   int
+	JavaInvokerVersion    string
+	NodeInvokerVersion    string
+	ShellInvokerVersion   string
+	Python2InvokerVersion string
+	Namespace             string
+	KafkaPodName          string
+	HTTPGatewayURL        string
+	DockerOrg             string
+	DockerUsername        string
+	DockerPassword        string
+	BaseDir               string
+	MessageRTTimeout      int
 }
 
 func InitSystemTestConfig() SystemTestConfig {
 
 	return SystemTestConfig{
-		JavaInvokerVersion: ensureEnv("SYS_TEST_JAVA_INVOKER_VERSION"),
-		NodeInvokerVersion: ensureEnv("SYS_TEST_NODE_INVOKER_VERSION"),
-		BaseDir:            ensureEnv("SYS_TEST_BASE_DIR"),
-		Namespace:          ensureEnv("SYS_TEST_NS"),
-		KafkaPodName:       ensureEnv("SYS_TEST_KAFKA_POD_NAME"),
-		HTTPGatewayURL:     ensureEnv("SYS_TEST_HTTP_GW_URL"),
-		DockerOrg:          ensureEnv("SYS_TEST_DOCKER_ORG"),
-		DockerUsername:     ensureEnv("SYS_TEST_DOCKER_USERNAME"),
-		DockerPassword:     ensureEnv("SYS_TEST_DOCKER_PASSWORD"),
-		MessageRTTimeout:   ensureEnvInt("SYS_TEST_MSG_RT_TIMEOUT_SEC"),
+		JavaInvokerVersion:    ensureEnv("SYS_TEST_JAVA_INVOKER_VERSION"),
+		NodeInvokerVersion:    ensureEnv("SYS_TEST_NODE_INVOKER_VERSION"),
+		ShellInvokerVersion:   ensureEnv("SYS_TEST_SHELL_INVOKER_VERSION"),
+		Python2InvokerVersion: ensureEnv("SYS_TEST_PYTHON2_INVOKER_VERSION"),
+		BaseDir:               ensureEnv("SYS_TEST_BASE_DIR"),
+		Namespace:             ensureEnv("SYS_TEST_NS"),
+		KafkaPodName:          ensureEnv("SYS_TEST_KAFKA_POD_NAME"),
+		HTTPGatewayURL:        ensureEnv("SYS_TEST_HTTP_GW_URL"),
+		DockerOrg:             ensureEnv("SYS_TEST_DOCKER_ORG"),
+		DockerUsername:        ensureEnv("SYS_TEST_DOCKER_USERNAME"),
+		DockerPassword:        ensureEnv("SYS_TEST_DOCKER_PASSWORD"),
+		MessageRTTimeout:      ensureEnvInt("SYS_TEST_MSG_RT_TIMEOUT_SEC"),
 	}
 }
 
@@ -157,6 +161,10 @@ func DockerTagAndPush(functionName string, imageName string) {
 
 func MvnCleanPackage(directory string) {
 	runSafely("Maven Build", directory, "./mvnw", "clean", "package")
+}
+
+func Run(directory string, command string, args ...string) {
+	runSafely("Run ["+command+"]", directory, command, args...)
 }
 
 func runSafely(description string, directory string, command string, args ...string) {
