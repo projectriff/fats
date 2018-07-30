ce1fce737d874f6521a0d18d45a2349723836410
Scott Andrews <sandrews@pivotal.io>
2018-07-30 16:47:44 -0400

    Update gcloud (take 2)

--- install_gke.sh
+++ install_gke.sh
@@ -4,11 +4,21 @@ source ./util.sh
 
 go get github.com/GoogleCloudPlatform/docker-credential-gcr
 
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
 gcloud config set project cf-spring-pfs-eng
 gcloud config set compute/zone us-central1-a
 gcloud config set disable_prompts True
-
-gcloud components update
 
 echo $GCLOUD_CLIENT_SECRET | base64 --decode > client-secret.json
 gcloud auth activate-service-account --key-file client-secret.json
