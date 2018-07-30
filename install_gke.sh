923195827404ed992e8d9d3e969cbe5cce11307f
Scott Andrews <sandrews@pivotal.io>
2018-07-30 16:42:58 -0400

    Update gcloud

--- install_gke.sh
+++ install_gke.sh
@@ -4,13 +4,15 @@ source ./util.sh
 
 go get github.com/GoogleCloudPlatform/docker-credential-gcr
 
-echo $GCLOUD_CLIENT_SECRET | base64 --decode > client-secret.json
-gcloud auth activate-service-account --key-file client-secret.json
-rm client-secret.json
-
 gcloud config set project cf-spring-pfs-eng
 gcloud config set compute/zone us-central1-a
 gcloud config set disable_prompts True
+
+gcloud components update
+
+echo $GCLOUD_CLIENT_SECRET | base64 --decode > client-secret.json
+gcloud auth activate-service-account --key-file client-secret.json
+rm client-secret.json
 
 gcloud container clusters create --num-nodes 2 $CLUSTER_NAME
 gcloud container clusters get-credentials $CLUSTER_NAME
