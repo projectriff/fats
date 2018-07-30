9f9205cc3f90722e5422af253995ab997af6ed53
Scott Andrews <sandrews@pivotal.io>
2018-07-30 18:01:38 -0400

    use recomended cluster config

--- install_gke.sh
+++ install_gke.sh
@@ -2,8 +2,6 @@
 
 source ./util.sh
 
-go get github.com/GoogleCloudPlatform/docker-credential-gcr
-
 # Create environment variable for correct distribution
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
@@ -24,9 +22,13 @@ echo $GCLOUD_CLIENT_SECRET | base64 --decode > client-secret.json
 gcloud auth activate-service-account --key-file client-secret.json
 rm client-secret.json
 
-gcloud container clusters create --num-nodes 2 $CLUSTER_NAME
-gcloud container clusters get-credentials $CLUSTER_NAME
-docker-credential-gcr configure-docker
+gcloud container clusters create $CLUSTER_NAME \
+  --cluster-version=latest \
+  --machine-type=n1-standard-4 \
+  --enable-autoscaling --min-nodes=1 --max-nodes=3 \
+  --enable-autorepair \
+  --scopes=service-control,service-management,compute-rw,storage-ro,cloud-platform,logging-write,monitoring-write,pubsub,datastore \
+  --num-nodes=3
 
 kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole=cluster-admin \
