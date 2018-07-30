3db4fd2fcb5ddd45677e29ee7d3470acd523e550
Scott Andrews <scott@andrews.me>
2018-07-30 16:09:26 -0700

    riff 0.1 (#25)
    * Use new riff cli and knative
    
    * Update gcloud
    
    * Update gcloud (take 2)
    
    * Setup clusteradmin
    
    * skip riff cleanup
    
    * Update uppercase function
    
    * Add logging before long pause
    
    * use recomended cluster config
    
    * imporve logging arround service invoke
    
    * Give more time after service reports ready

--- install_gke.sh
+++ install_gke.sh
@@ -2,16 +2,34 @@
 
 source ./util.sh
 
-go get github.com/GoogleCloudPlatform/docker-credential-gcr
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
-echo $GCLOUD_CLIENT_SECRET | base64 --decode > client-secret.json
-gcloud auth activate-service-account --key-file client-secret.json
-rm client-secret.json
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
 
 gcloud config set project cf-spring-pfs-eng
 gcloud config set compute/zone us-central1-a
 gcloud config set disable_prompts True
 
-gcloud container clusters create --num-nodes 2 $CLUSTER_NAME
-gcloud container clusters get-credentials $CLUSTER_NAME
-docker-credential-gcr configure-docker
+echo $GCLOUD_CLIENT_SECRET | base64 --decode > client-secret.json
+gcloud auth activate-service-account --key-file client-secret.json
+rm client-secret.json
+
+gcloud container clusters create $CLUSTER_NAME \
+  --cluster-version=latest \
+  --machine-type=n1-standard-4 \
+  --enable-autoscaling --min-nodes=1 --max-nodes=3 \
+  --enable-autorepair \
+  --scopes=service-control,service-management,compute-rw,storage-ro,cloud-platform,logging-write,monitoring-write,pubsub,datastore \
+  --num-nodes=3
+
+kubectl create clusterrolebinding cluster-admin-binding \
+  --clusterrole=cluster-admin \
+  --user=$(gcloud config get-value core/account)
