e380c11cb44cada71f8bac8a43bcf55ddba192cd
Scott Andrews <sandrews@pivotal.io>
2018-08-07 20:57:29 -0400

    cleanup service account access keys

--- cleanup-gke.sh
+++ cleanup-gke.sh
@@ -3,6 +3,7 @@
 source ./util.sh
 
 gcloud container clusters delete $CLUSTER_NAME
+gcloud iam service-accounts delete "push-image@$(gcloud config get-value project).iam.gserviceaccount.com"
 gcloud compute firewall-rules list --filter $CLUSTER_NAME --format="table(name)" | \
   tail -n +2 | \
   xargs --no-run-if-empty gcloud compute firewall-rules delete
