d1cd53579371f52b97ad5d095cdfa7f1feafc992
Scott Andrews <scott@andrews.me>
2018-08-08 19:54:43 -0400

    Fix gcr-push credentials (#28)
    * Use stock gcp creds rather than attempting to generate on demand creds
    
    * remove unused var
    
    * fd fix

--- install_riff.sh
+++ install_riff.sh
@@ -4,6 +4,23 @@ source ./util.sh
 
 go get github.com/projectriff/riff
 
+echo "Create auth secret"
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: gcr-creds
+  annotations:
+    build.knative.dev/docker-0: https://us.gcr.io
+    build.knative.dev/docker-1: https://gcr.io
+    build.knative.dev/docker-2: https://eu.gcr.io
+    build.knative.dev/docker-3: https://asia.gcr.io
+type: kubernetes.io/basic-auth
+data:
+  username: $(echo -n "_json_key" | openssl base64 -a -A) # Should be X2pzb25fa2V5
+  password: $(echo $GCLOUD_CLIENT_SECRET)
+EOF
+
 riff system install
 riff namespace init default --secret gcr-creds
 
