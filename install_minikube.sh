a3a486ba4763d5e78167210fee0867b095862352
Scott Andrews <sandrews@pivotal.io>
2018-08-26 18:37:33 -0400

    bootstrapper=localkube

--- install_minikube.sh
+++ install_minikube.sh
@@ -17,6 +17,7 @@ fi
 sudo minikube start --memory=8192 --cpus=4 \
   --kubernetes-version=v1.10.5 \
   --vm-driver=none \
+  --bootstrapper=localkube \
   --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
   --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
   --extra-config=apiserver.admission-control="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
