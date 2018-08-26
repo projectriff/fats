536c64f06aa893e4b6bcd20c0e72caf093417862
Scott Andrews <sandrews@pivotal.io>
2018-08-26 18:48:52 -0400

    wait for minikube to be ready

--- install_minikube.sh
+++ install_minikube.sh
@@ -31,6 +31,11 @@ sudo minikube start --memory=8192 --cpus=4 \
 # Fix the kubectl context, as it's often stale.
 minikube update-context
 
+# Wait for Kubernetes to be up and ready.
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
+  until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
+
+
 echo "Create auth secret"
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
