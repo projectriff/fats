19ce0f107369c38b7f7273f1b6c2bb989265f95a
Scott Andrews <sandrews@pivotal.io>
2018-09-20 16:28:13 -0400

    Update invoker names
    and other polish

--- functions/uppercase/run.sh
+++ functions/uppercase/run.sh
@@ -2,10 +2,11 @@
 
 dir=`dirname "${BASH_SOURCE[0]}"`
 function=`basename $dir`
+date="date -u +%Y-%m-%dT%H:%M:%SZ"
 
-for runtime in command java java-buildpack java-buildpack-local node; do
-  pushd $dir/$runtime
-    function_name="fats-$function-$runtime"
+for invoker in command jar java java-local node; do
+  pushd $dir/$invoker
+    function_name="fats-$function-$invoker"
     function_version="${CLUSTER_NAME}"
     image="${USER_ACCOUNT}/${function_name}:${function_version}"
     input_data="hello"
@@ -15,9 +16,9 @@ for runtime in command java java-buildpack java-buildpack-local node; do
       args=`cat .fats/create`
     fi
 
-    if [ -e '.fats/runtime' ]; then
-      # overwrite runtime
-      runtime=`cat .fats/runtime`
+    if [ -e '.fats/invoker' ]; then
+      # overwrite invoker
+      invoker=`cat .fats/invoker`
     fi
 
     kail --label "function=$function_name" > $function_name.logs &
@@ -27,22 +28,22 @@ for runtime in command java java-buildpack java-buildpack-local node; do
     kail_controller_pid=$!
 
     # create function
-    riff function create $runtime $function_name $args \
-      --image $image
+    echo "[`$date`] Creating $function_name as $invoker:"
+    riff function create $invoker $function_name $args --image $image
 
     # wait for function to build and deploy
-    echo "Waiting for function to become ready"
-    until kube_ready \
-      'pods' \
+    echo "[`$date`] Waiting for $function_name to become ready:"
+     until kube_ready \
+      'services.serving.knative.dev' \
       'default' \
-      "serving.knative.dev/configuration=${function_name}" \
-      '{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}' \
-      'Ready=True' \
+      "${function_name}" \
+      ';{range @.status.conditions[*]}{@.type}={@.status};{end}' \
+      ';Ready=True;' \
     ; do sleep 1; done
-    # TODO reduce/eliminate this sleep
-    sleep 10
+    sleep 5
 
     # invoke function
+    echo "[`$date`] Invoking $function_name:"
     riff service invoke $function_name -- \
       -H "Content-Type: text/plain" \
       -d $input_data \
@@ -66,7 +67,7 @@ for runtime in command java java-buildpack java-buildpack-local node; do
       echo -e "Controller Logs:"
       cat $function_name.controller.logs
       echo -e ""
-      echo -e "${RED}Function did not produce expected result${NC}";
+      echo -e "[`$date`] ${RED}Function did not produce expected result${NC}:";
       echo -e "   expected: $expected_data"
       echo -e "   actual: $actual_data"
       exit 1
--- util.sh
+++ util.sh
@@ -10,10 +10,19 @@ NC='\033[0m' # No Color
 kube_ready() {
   resource=$1
   namespace=$2
-  label=$3
   jsonpath=$4
   pattern=$5
 
+if [[ $3 = *"="* ]]; then
+  label=$3
+
   kubectl get $resource --namespace $namespace -l $label \
     -o jsonpath="$jsonpath" 2>&1 | grep -qE $pattern
+else
+  name=$3
+
+  kubectl get $resource --namespace $namespace $name \
+    -o jsonpath="$jsonpath" 2>&1 | grep -qE $pattern
+fi
+
 }
