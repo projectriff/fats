0684ec4db1cd0ec74a17064947d544593fb7ce87
Scott Andrews <sandrews@pivotal.io>
2018-10-01 16:50:47 -0400

    Flatten channels run script
    - create the correlator once for all channel tests
    - avoid extra looping over channel invokers since there is only one
    - consolidate function and channel run scripts into a single top level
      script

--- channels/run.sh
+++ channels/run.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-source ./util.sh
-source ./init.sh $CLUSTER
+date="date -u +%Y-%m-%dT%H:%M:%SZ"
+service_name='correlator'
+
+# install correlator
+riff service create $service_name --image projectriff/correlator:fats
+
+# wait for correlator to deploy
+echo "[`$date`] Waiting for $service_name to become ready:"
+  until kube_ready \
+  'services.serving.knative.dev' \
+  'default' \
+  "${service_name}" \
+  ';{range @.status.conditions[*]}{@.type}={@.status};{end}' \
+  ';Ready=True;' \
+; do sleep 1; done
+
 
 for test in correlated-hello; do
   dir=`dirname "${BASH_SOURCE[0]}"`
   echo "Current eventing scenario: $test"
-  source $dir/$test/run.sh
+  source $dir/$test/run.sh $service_name
 done
+
+# cleanup correlator
+riff service delete $service_name
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ./util.sh
+source ./init.sh $CLUSTER
+
+source ./functions/run.sh
+source ./channels/run.sh
