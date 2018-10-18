f55c830571d2ae17658d9aee5b9629da42bc0d6f
Florent Biville <fbiville@pivotal.io>
2018-10-18 13:39:10 +0200

    Build against minikube/latest again
    Fixes #39

--- install_minikube.sh
+++ install_minikube.sh
@@ -6,25 +6,23 @@ source ./util.sh
 
 export CHANGE_MINIKUBE_NONE_USER=true
 
-VERSION='v0.28.2'
 # install minikube if needed
 if hash minikube 2>/dev/null; then
   echo "Skipping minikube install"
 else
   echo "Installing minikube"
-  curl -Lo minikube https://storage.googleapis.com/minikube/releases/$VERSION/minikube-linux-amd64 && \
-    chmod +x minikube && sudo mv minikube /usr/local/bin/
+  curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
+  chmod +x minikube && \
+  sudo mv minikube /usr/local/bin/
 fi
 
 # Make root mounted as rshared to fix kube-dns issues.
 sudo mount --make-rshared /
 
 sudo minikube start --memory=8192 --cpus=4 \
-  --kubernetes-version=v1.10.0 \
   --vm-driver=none \
-  --bootstrapper=localkube \
-  --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
-  --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
+  --bootstrapper=kubeadm \
+  --kubernetes-version=v1.12.0 \
   --extra-config=apiserver.admission-control="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
 
 # Fix the kubectl context, as it's often stale.
