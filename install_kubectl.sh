8aaab3190070a5b329ca05b63d8ddf42b09d5421
Scott Andrews <sandrews@pivotal.io>
2018-11-02 14:45:31 -0400

    bump kubectl version

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -2,7 +2,7 @@
 
 source ./util.sh
 
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.2/bin/linux/amd64/kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.12.2/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
