36b03ca137fd482ce27b96c3dccc587e7bd67ef9
Scott Andrews <scott@andrews.me>
2018-11-07 16:37:53 -0500

    Composable FATS (#57)
    Allow FATS scripts to be run selectively without needing to run everything.
    
    `function/helpers.sh` contains functions to create, invoke and delete functions. As well a a run function that orchestrates all three.

--- functions/run.sh
+++ functions/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+dir=`dirname "${BASH_SOURCE[0]}"`
+
+source $dir/helpers.sh
+
 for test in uppercase; do
-  dir=`dirname "${BASH_SOURCE[0]}"`
   echo "Current function scenario: $test"
   source $dir/$test/run.sh
 done
--- functions/uppercase/run.sh
+++ functions/uppercase/run.sh
@@ -4,63 +4,11 @@ dir=`dirname "${BASH_SOURCE[0]}"`
 function=`basename $dir`
 
 for invoker in command jar java java-local node; do
-  pushd $dir/$invoker
-    function_name="fats-$function-$invoker"
-    function_version="${CLUSTER_NAME}"
-    image="${USER_ACCOUNT}/${function_name}:${function_version}"
-    input_data="hello"
+  path="$dir/$invoker"
+  function_name="fats-$function-$invoker"
+  image="${USER_ACCOUNT}/${function_name}:${CLUSTER_NAME}"
+  input_data="hello"
+  expected_data="HELLO"
 
-    args=""
-    if [ -e '.fats/create' ]; then
-      args=`cat .fats/create`
-    fi
-
-    if [ -e '.fats/invoker' ]; then
-      # overwrite invoker
-      invoker=`cat .fats/invoker`
-    fi
-
-    kail --ns $NAMESPACE --label "function=$function_name" > $function_name.logs &
-    kail_function_pid=$!
-
-    kail --ns knative-serving > $function_name.controller.logs &
-    kail_controller_pid=$!
-
-    # create function
-    fats_echo "Creating $function_name as $invoker:"
-    riff function create $invoker $function_name $args --image $image --namespace $NAMESPACE --verbose
-    # TODO reduce/eliminate this sleep
-    sleep 5
-
-    # invoke function
-    fats_echo "Invoking $function_name:"
-    riff service invoke $function_name --namespace $NAMESPACE -- \
-      -H "Content-Type: text/plain" \
-      -d $input_data \
-      -v | tee $function_name.out
-
-    # add a new line after invoke, but without impacting the curl output
-    echo ""
-
-    expected_data="HELLO"
-    actual_data=`cat $function_name.out | tail -1`
-
-    # cleanup resources
-    kill $kail_function_pid $kail_controller_pid
-    riff service delete $function_name --namespace $NAMESPACE
-    fats_delete_image $image
-
-    if [ "$actual_data" != "$expected_data" ]; then
-      fats_echo "Function Logs:"
-      cat $function_name.logs
-      echo ""
-      fats_echo "Controller Logs:"
-      cat $function_name.controller.logs
-      echo ""
-      fats_echo "${RED}Function did not produce expected result${NC}:";
-      echo "   expected: $expected_data"
-      echo "   actual: $actual_data"
-      exit 1
-    fi
-  popd
+  run_function $path $invoker $function_name $image $input_data $expected_data
 done
