db592c7ff1eb34deebdee85b9792f5cebc62e7ed
Swapnil Bawaskar <sbawaskar@pivotal.io>
2018-11-16 07:52:58 -0800

    setting environment name for cleanup script

--- cleanup_pks.sh
+++ cleanup_pks.sh
@@ -4,7 +4,7 @@ source ./util.sh
 
 fats_setup_gcloud
 
-
+export TS_G_ENV=$(echo $TOOLSMITH_ENV | base64 --decode | jq -r .name)
 gcloud compute forwarding-rules delete ${TS_G_ENV}-${CLUSTER_NAME}-fw --region=${CLOUDSDK_COMPUTE_REGION}
 gcloud compute target-instances delete ${TS_G_ENV}-${CLUSTER_NAME}-ti --zone=${CLOUDSDK_COMPUTE_ZONE}
 gcloud compute addresses delete ${TS_G_ENV}-${CLUSTER_NAME}-ip --region=${CLOUDSDK_COMPUTE_REGION}
