d0d34146aa78d97794254f84144bcc454637b2ca
Scott Andrews <sandrews@pivotal.io>
2018-11-26 18:55:26 -0500

    restore secret

--- init_minikube.sh
+++ init_minikube.sh
@@ -20,6 +20,7 @@ fats_delete_image() {
 fats_create_push_credentials() {
   namespace=$1
 
+  # TODO riff requires a secret be provided to `riff namespace init`, but we don't actually need it for a local registry
   echo "Create auth secret"
   cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -27,6 +28,11 @@ kind: Secret
 metadata:
   name: push-credentials
   namespace: $(echo -n "$namespace")
+  annotations:
+    build.knative.dev/docker-0: https://index.docker.io/v1/
 type: kubernetes.io/basic-auth
+data:
+  username: $(echo -n "$DOCKER_USERNAME" | openssl base64 -a -A)
+  password: $(echo -n "$DOCKER_PASSWORD" | openssl base64 -a -A)
 EOF
 }
