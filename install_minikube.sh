24ce4821691c9923eb731a9f9a664fa2a9ebe926
Scott Andrews <sandrews@pivotal.io>
2018-11-26 19:56:29 -0500

    more progress

--- install_minikube.sh
+++ install_minikube.sh
@@ -34,10 +34,10 @@ JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.ty
   until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
 
 # Enable local registry
-sudo minikube addons list
+echo "Installing a local registry"
 wait_pod_selector_ready kubernetes.io/minikube-addons=addon-manager kube-system
 sudo minikube addons enable registry
 wait_pod_selector_ready kubernetes.io/minikube-addons=registry kube-system
 registry_ip=$(kubectl get svc --namespace kube-system -l "kubernetes.io/minikube-addons=registry" -o jsonpath="{.items[0].spec.clusterIP}")
 # because we are running with --vm-driver=none the local host is minikube's host
-sudo su -c 'echo "$registry_ip       registry.kube-system.svc.cluster.local" >> /etc/hosts'
+sudo su -c "echo \"$registry_ip       registry.kube-system.svc.cluster.local\" >> /etc/hosts"
