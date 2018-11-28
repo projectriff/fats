debed24d1d96ba77e0d057fa9f2bcb44732d5a6f
Scott Andrews <sandrews@pivotal.io>
2018-11-28 10:40:36 -0500

    Use --no-secret for minikube with local registry

--- init_minikube.sh
+++ init_minikube.sh
@@ -2,7 +2,7 @@
 
 export USER_ACCOUNT="registry.kube-system.svc.cluster.local/u"
 export SYSTEM_INSTALL_FLAGS="${SYSTEM_INSTALL_FLAGS:---node-port}"
-export NAMESPACE_INIT_FLAGS="${NAMESPACE_INIT_FLAGS:---secret push-credentials}"
+export NAMESPACE_INIT_FLAGS="${NAMESPACE_INIT_FLAGS:---no-secret}"
 
 wait_for_ingress_ready() {
   name=$1
@@ -20,19 +20,5 @@ fats_delete_image() {
 fats_create_push_credentials() {
   namespace=$1
 
-  # TODO riff requires a secret be provided to `riff namespace init`, but we don't actually need it for a local registry
-  echo "Create auth secret"
-  cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Secret
-metadata:
-  name: push-credentials
-  namespace: $(echo -n "$namespace")
-  annotations:
-    build.knative.dev/docker-0: https://index.docker.io/v1/
-type: kubernetes.io/basic-auth
-data:
-  username: $(echo -n "$DOCKER_USERNAME" | openssl base64 -a -A)
-  password: $(echo -n "$DOCKER_PASSWORD" | openssl base64 -a -A)
-EOF
+  # nothing to do
 }
