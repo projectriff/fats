469bf56d1fdfb85b7a29d2aa21722b4f159b60e3
Scott Andrews <sandrews@pivotal.io>
2018-11-29 15:58:54 -0500

    cleanup orphaned pks resources

--- cleanup_pks.sh
+++ cleanup_pks.sh
@@ -18,11 +18,35 @@ pks delete-cluster ${TS_G_ENV}-${CLUSTER_NAME} --non-interactive --wait
 set +o errexit
 set +o pipefail
 
-cluster_prefix=${1:-fats}
+cluster_prefix="${TS_G_ENV}-`$CLUSTER_NAME | cut -d '-' -f1`"
 before=`date -d @$(( $(date +"%s") - 24*3600)) -u +%Y-%m-%dT%H:%M:%SZ` # yesterday
 
-gcloud config unset compute/region
-gcloud config unset compute/zone
+fats_echo "Cleanup orphaned clusters"
+pks clusters
+pks clusters --json | jq -r '.[].name' | \
+  xargs -L 1 --no-run-if-empty pks delete-cluster --non-interactive --wait
+pks clusters
+
+fats_echo "Cleanup orphaned forwarding rules"
+gcloud compute forwarding-rules list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+gcloud compute forwarding-rules list --filter="name ~ ^$cluster_prefix- AND createTime < $before" --format="table[no-heading](name, region)" | \
+  sed 's/ / --region /2' | \
+  xargs -L 1 --no-run-if-empty gcloud compute forwarding-rules delete
+gcloud compute forwarding-rules list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+
+fats_echo "Cleanup orphaned target instances"
+gcloud compute target-instances list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+gcloud compute target-instances list --filter="name ~ ^$cluster_prefix- AND createTime < $before" --format="table[no-heading](name, zone)" | \
+  sed 's/ / --zone /2' | \
+  xargs -L 1 --no-run-if-empty gcloud compute target-instances delete
+gcloud compute target-instances list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+
+fats_echo "Cleanup orphaned addresses"
+gcloud compute addresses list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+gcloud compute addresses list --filter="name ~ ^$cluster_prefix- AND createTime < $before" --format="table[no-heading](name, region)" | \
+  sed 's/ / --region /2' | \
+  xargs -L 1 --no-run-if-empty gcloud compute addresses delete
+gcloud compute addresses list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
 
 fats_echo "Cleanup orphaned disks"
 gcloud compute disks list --filter="name ~ ^disk- AND createTime < $before"
