61f5149cead30430725d13fd5069df65e0be7e17
Scott Andrews <scott@andrews.me>
2018-11-30 13:24:46 -0500

    Take out the trash (#72)
    GKE and Bosh seem to orphan a number of resources. We should clean them
    up periodically in order to avoid exceeding quota limits.
    
    Fixes #67

--- cleanup_pks.sh
+++ cleanup_pks.sh
@@ -12,3 +12,45 @@ gcloud compute target-instances delete ${TS_G_ENV}-${CLUSTER_NAME}-ti --zone=${g
 gcloud compute addresses delete ${TS_G_ENV}-${CLUSTER_NAME}-ip --region=${gcp_region}
 
 pks delete-cluster ${TS_G_ENV}-${CLUSTER_NAME} --non-interactive --wait
+
+# delete orphaned resources
+
+set +o errexit
+set +o pipefail
+
+cluster_prefix="${TS_G_ENV}-`echo $CLUSTER_NAME | cut -d '-' -f1`"
+before=`date -d @$(( $(date +"%s") - 24*3600)) -u +%Y-%m-%dT%H:%M:%SZ` # yesterday
+
+fats_echo "Cleanup orphaned clusters"
+pks clusters
+pks clusters --json | jq -r '.[].name' | \
+  xargs -L 1 --no-run-if-empty pks delete-cluster --non-interactive --wait
+pks clusters
+
+fats_echo "Cleanup orphaned forwarding rules"
+gcloud compute forwarding-rules list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+gcloud compute forwarding-rules list --filter="name ~ ^$cluster_prefix- AND createTime < $before" --format="table[no-heading](name, region)" | \
+  sed 's/ / --region /2' | \
+  xargs -L 1 --no-run-if-empty gcloud compute forwarding-rules delete
+gcloud compute forwarding-rules list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+
+fats_echo "Cleanup orphaned target instances"
+gcloud compute target-instances list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+gcloud compute target-instances list --filter="name ~ ^$cluster_prefix- AND createTime < $before" --format="table[no-heading](name, zone)" | \
+  sed 's/ / --zone /2' | \
+  xargs -L 1 --no-run-if-empty gcloud compute target-instances delete
+gcloud compute target-instances list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+
+fats_echo "Cleanup orphaned addresses"
+gcloud compute addresses list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+gcloud compute addresses list --filter="name ~ ^$cluster_prefix- AND createTime < $before" --format="table[no-heading](name, region)" | \
+  sed 's/ / --region /2' | \
+  xargs -L 1 --no-run-if-empty gcloud compute addresses delete
+gcloud compute addresses list --filter="name ~ ^$cluster_prefix- AND createTime < $before"
+
+fats_echo "Cleanup orphaned disks"
+gcloud compute disks list --filter="name ~ ^disk- AND createTime < $before"
+gcloud compute disks list --filter="name ~ ^disk- AND createTime < $before" --format="table[no-heading](name, zone)" | \
+  sed 's/ / --zone /2' | \
+  xargs -L 1 --no-run-if-empty gcloud compute disks delete
+gcloud compute disks list --filter="name ~ ^disk- AND createTime < $before"
