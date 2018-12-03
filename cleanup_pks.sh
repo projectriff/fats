401c7848a4f946077c34bd7efd0292e5ec1dd51f
Scott Andrews <sandrews@pivotal.io>
2018-12-03 18:12:53 -0500

    Get zone from PKS master node
    PKS vms are created by bosh in the zone bosh is configued to use. It may
    not be the same zone that gcloud is defaulted to. In that case, when
    configuring external access to the k8s api server, the zone of the
    master node must be used.
    
    Also configuring external DNS, for kicks

--- cleanup_pks.sh
+++ cleanup_pks.sh
@@ -5,11 +5,15 @@ source `dirname "${BASH_SOURCE[0]}"`/util.sh
 TS_G_ENV=$(echo $TOOLSMITH_ENV | base64 --decode | jq -r .name)
 
 gcp_region=`gcloud config get-value compute/region`
-gcp_zone=`gcloud config get-value compute/zone`
+lb_ip=`gcloud compute addresses list --filter="name=(${TS_G_ENV}-${CLUSTER_NAME}-ip)" --format=json | jq -r .[0].address`
 
-gcloud compute forwarding-rules delete ${TS_G_ENV}-${CLUSTER_NAME}-fw --region=${gcp_region}
-gcloud compute target-instances delete ${TS_G_ENV}-${CLUSTER_NAME}-ti --zone=${gcp_zone}
-gcloud compute addresses delete ${TS_G_ENV}-${CLUSTER_NAME}-ip --region=${gcp_region}
+gcloud dns record-sets transaction start --zone=${TS_G_ENV}-zone
+gcloud dns record-sets transaction remove ${lb_ip} --name=${pks_hostname}. --ttl=300 --type=A --zone=${TS_G_ENV}-zone
+gcloud dns record-sets transaction execute --zone=${TS_G_ENV}-zone
+
+gcloud compute forwarding-rules delete ${TS_G_ENV}-${CLUSTER_NAME}-fr --region ${gcp_region}
+gcloud compute target-pools delete ${TS_G_ENV}-${CLUSTER_NAME}-tp
+gcloud compute addresses delete ${TS_G_ENV}-${CLUSTER_NAME}-ip
 
 pks delete-cluster ${TS_G_ENV}-${CLUSTER_NAME} --non-interactive --wait
 
