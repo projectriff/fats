9aeec613c2597877024081a95844b7722a1ef42a
Scott Andrews <sandrews@pivotal.io>
2018-12-07 10:54:02 -0500

    Install tools in sub shells
    Refs https://github.com/projectriff/fats/pull/79#discussion_r239809875

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 if [ ! -f `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed ]; then
   echo "Installing $1"
-  source `dirname "${BASH_SOURCE[0]}"`/tools/$1.sh
+  `dirname "${BASH_SOURCE[0]}"`/tools/$1.sh
   touch `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed
 fi
