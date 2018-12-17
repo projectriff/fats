4c4bba9b5d94e0f73c805ed0fb8e34df46a8fba8
Scott Andrews <sandrews@pivotal.io>
2018-12-17 10:06:26 -0500

    Compile riff from master

--- tools/riff.sh
+++ tools/riff.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-riff_dir=`mktemp -d riff.XXXX`
-
-curl -L https://github.com/projectriff/riff/releases/download/v0.2.0/riff-linux-amd64.tgz \
-  | tar xz -C $riff_dir
-chmod +x $riff_dir/riff
-sudo mv $riff_dir/riff /usr/local/bin/
-
-rm -rf $riff_dir
+go get github.com/projectriff/riff
