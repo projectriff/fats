e5aff436565593401b42e9c7ac63fe7453929257
Scott Andrews <sandrews@pivotal.io>
2018-12-27 15:32:33 -0500

    use az install docs

--- tools/az.sh
+++ tools/az.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" \
+# from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-apt?view=azure-cli-latest
+
+sudo apt-get install apt-transport-https lsb-release software-properties-common -y
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" \
   | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+  --keyserver packages.microsoft.com \
+  --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+
+sudo apt-get update
+sudo apt-get install azure-cli
 
 # login
 az login --service-principal \
