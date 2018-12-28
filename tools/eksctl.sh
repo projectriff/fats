fef666accabe9e642c282b60ef7bc3ac3d619b53
Scott Andrews <sandrews@pivotal.io>
2018-12-28 13:37:13 -0500

    Update eksctl to 0.1.16

--- tools/eksctl.sh
+++ tools/eksctl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-eksctl_version="0.1.15"
+eksctl_version="0.1.16"
 eksctl_dir=`mktemp -d eksctl.XXXX`
 
 curl -s -L "https://github.com/weaveworks/eksctl/releases/download/${eksctl_version}/eksctl_Linux_amd64.tar.gz" \
