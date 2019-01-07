447f0ef8d8359f6de57e392b84d00fa3e33c8f0b
Scott Andrews <scott@andrews.me>
2019-01-07 12:51:58 -0500

    EKS cluster (and almost ECR registry) support (#91)
    EKS support is limited to k8s 1.10.
    
    Fully ECR support is blocked by https://github.com/knative/serving/issues/1996

---
+++ tools/eksctl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install aws cli
+source `dirname "${BASH_SOURCE[0]}"`/../install.sh aws
+source `dirname "${BASH_SOURCE[0]}"`/../install.sh aws-iam-authenticator
+
+eksctl_version="0.1.16"
+eksctl_dir=`mktemp -d eksctl.XXXX`
+
+curl -s -L "https://github.com/weaveworks/eksctl/releases/download/${eksctl_version}/eksctl_Linux_amd64.tar.gz" \
+  | tar xz -C $eksctl_dir
+chmod +x $eksctl_dir/eksctl
+sudo mv $eksctl_dir/eksctl /usr/local/bin
+
+rm -rf $eksctl_dir
