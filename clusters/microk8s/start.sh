83e4577ac088488795f09ac00f6725c2dc7f828e
Scott Andrews <sandrews@pivotal.io>
2019-01-15 16:24:31 -0500

    try k8s 1.11

--- clusters/microk8s/start.sh
+++ clusters/microk8s/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "install microk8s"
-sudo snap install microk8s --channel=1.13/stable --classic
+sudo snap install microk8s --channel=1.11/stable --classic
 echo "wait for microk8s"
 microk8s.status --wait-ready
 echo "enable dns storage"
