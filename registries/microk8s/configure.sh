3e12af19dff1310533b1b1c9ef5d336983ad101b
Scott Andrews <sandrews@pivotal.io>
2019-01-15 15:48:51 -0500

    update microk8s registry to use fats_image_repo

--- registries/microk8s/configure.sh
+++ registries/microk8s/configure.sh
@@ -3,6 +3,12 @@
 IMAGE_REPOSITORY_PREFIX="localhost:32000"
 NAMESPACE_INIT_FLAGS="${NAMESPACE_INIT_FLAGS:-} --no-secret"
 
+fats_image_repo() {
+  local function_name=$1
+
+  echo -n "${IMAGE_REPOSITORY_PREFIX}/${function_name}:${CLUSTER_NAME}"
+}
+
 fats_delete_image() {
   image=$1
 
