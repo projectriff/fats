1f1509cb3a29d374aafa45cbb257c33ea99b2a14
Scott Andrews <sandrews@pivotal.io>
2019-03-01 20:05:07 -0500

    Remove knative-ingressgateway
    Knative 0.4.0 removed their custom ingress gateway and instead use the default Istio-ingressgateway

--- tests/run.sh
+++ tests/run.sh
@@ -16,7 +16,6 @@ riff system install $SYSTEM_INSTALL_FLAGS
 
 # health checks
 echo "Checking for ready pods"
-wait_pod_selector_ready 'knative=ingressgateway' 'istio-system'
 wait_pod_selector_ready 'app=controller' 'knative-serving'
 wait_pod_selector_ready 'app=webhook' 'knative-serving'
 wait_pod_selector_ready 'app=build-controller' 'knative-build'
@@ -26,7 +25,7 @@ wait_pod_selector_ready 'app=webhook' 'knative-eventing'
 wait_pod_selector_ready 'clusterChannelProvisioner=in-memory-channel,role=controller' 'knative-eventing'
 wait_pod_selector_ready 'clusterChannelProvisioner=in-memory-channel,role=dispatcher' 'knative-eventing'
 echo "Checking for ready ingress"
-wait_for_ingress_ready 'knative-ingressgateway' 'istio-system'
+wait_for_ingress_ready 'istio-ingressgateway' 'istio-system'
 
 # setup namespace
 kubectl create namespace $NAMESPACE
