f1821b94ccb0044d83d16bd7dacdbcb48da0363a
Scott Andrews <sandrews@pivotal.io>
2019-03-08 15:29:11 -0500

    Update minikube to v0.35

--- tools/minikube.sh
+++ tools/minikube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minikube_version="v0.30.0"
+minikube_version="v0.35.0"
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/$minikube_version/minikube-linux-amd64
 chmod +x minikube
