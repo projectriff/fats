67aedcf310da7a7b6f749508166f17ba37bcd27b
Scott Andrews <sandrews@pivotal.io>
2019-03-29 17:05:44 -0400

    Apply minikube patch
    as sugested by https://blog.gripdev.xyz/2018/10/19/kubernetes-integration-testing-minikube-azure-pipelines-happy/

--- clusters/minikube/start.sh
+++ clusters/minikube/start.sh
@@ -4,12 +4,19 @@
 
 export CHANGE_MINIKUBE_NONE_USER=true
 
+# Make root mounted as rshared to fix kube-dns issues.
+sudo mount --make-rshared /
+
 sudo minikube start --memory=8192 --cpus=4 \
   --kubernetes-version=v1.12.3 \
   --vm-driver=none \
   --bootstrapper=kubeadm \
   --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook" \
   --insecure-registry registry.kube-system.svc.cluster.local
+
+# Fix permissions issue in AzurePipelines
+sudo chmod --recursive 777 $HOME/.minikube
+sudo chmod --recursive 777 $HOME/.kube
 
 # Fix the kubectl context, as it's often stale.
 sudo minikube update-context
