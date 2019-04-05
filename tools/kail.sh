ccee14fcf6788043c45e46ef4d54d8cf8bd6af82
Scott Andrews <sandrews@pivotal.io>
2019-04-05 18:24:32 -0400

    build kail with make

--- tools/kail.sh
+++ tools/kail.sh
@@ -3,7 +3,7 @@
 if [ "$machine" == "MinGw" ]; then
   # binaries are not available for windows, we need to build from source
   go get -d github.com/boz/kail
-  go install github.com/boz/kail/cmd/kail
+  (cd $GOPATH/src/github.com/boz/kail && make install-deps && make)
 else
   mkdir -p kail
   curl -L https://github.com/boz/kail/releases/download/v0.7.0/kail_0.7.0_linux_amd64.tar.gz \
