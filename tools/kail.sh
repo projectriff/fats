afafac0bdb72de2bf07240ae2f824cb8a6e87716
Scott Andrews <sandrews@pivotal.io>
2019-04-05 19:16:55 -0400

    stub kail to make forward progress

--- tools/kail.sh
+++ tools/kail.sh
@@ -2,14 +2,19 @@
 
 if [ "$machine" == "MinGw" ]; then
   # binaries are not available for windows, we need to build from source
-  export GOPATH=$(go env GOPATH)
-  go get -d github.com/boz/kail
-  pushd $GOPATH/src/github.com/boz/kail
-    git checkout v0.6.0
-    make install-deps
-    make
-    mv kail.exe /usr/bin/
-  popd
+  # export GOPATH=$(go env GOPATH)
+  # go get -d github.com/boz/kail
+  # pushd $GOPATH/src/github.com/boz/kail
+  #   git checkout v0.7.0
+  #   make install-deps
+  #   make
+  #   mv kail.exe /usr/bin/
+  # popd
+
+  # stub out kail for now
+  sudo() {
+    return
+  }
 else
   mkdir -p kail
   curl -L https://github.com/boz/kail/releases/download/v0.7.0/kail_0.7.0_linux_amd64.tar.gz \
