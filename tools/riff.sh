6c360525a80819a89bc69e2697a61d64f691c3cc
Scott Andrews <sandrews@pivotal.io>
2019-04-08 16:59:11 -0400

    pin riff client to a compatible version

--- tools/riff.sh
+++ tools/riff.sh
@@ -2,7 +2,7 @@
 
 riff_dir=`mktemp -d riff.XXXX`
 
-curl -L https://storage.googleapis.com/projectriff/riff-cli/releases/latest/riff-linux-amd64.tgz \
+curl -L https://storage.googleapis.com/projectriff/riff-cli/releases/v0.3.0-snapshot/riff-linux-amd64.tgz \
   | tar xz -C $riff_dir
 chmod +x $riff_dir/riff
 sudo mv $riff_dir/riff /usr/local/bin/
