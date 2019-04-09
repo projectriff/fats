67289ec0d044481c366c92115412cb7db85f5e30
Scott Andrews <sandrews@pivotal.io>
2019-04-09 11:52:05 -0400

    Add docker-credential-gcloud to path

--- tools/gcloud.sh
+++ tools/gcloud.sh
@@ -13,6 +13,11 @@ EOF
 
 "/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/gsutil" \$@
 EOF
+  cat <<EOF > /usr/bin/docker-credential-gcloud
+#!/bin/bash
+
+"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/docker-credential-gcloud" \$@
+EOF
 else
   # Create environment variable for correct distribution
   export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
