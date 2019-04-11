2b477501ded876c7f2e497ee30b91e13ac468a61
Scott Andrews <sandrews@pivotal.io>
2019-04-11 15:53:09 -0400

    =fix *.cmd mapping on windows

--- tools/gcloud.sh
+++ tools/gcloud.sh
@@ -11,13 +11,13 @@ if hash choco 2>/dev/null; then
 EOF
 
   # expose all gcloudsdk *.cmd into the path
-  while read cmd; do
-    cat <<EOF > /usr/bin/$(echo ${cmd} | cut -d. -f1)
+  while read -r cmd; do
+    cat <<EOF > /usr/bin/${cmd}
 #!/bin/bash
 
-"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/${cmd}" \$@
+"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/${cmd}.cmd" \$@
 EOF
-  done < $(ls -l "/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/" | grep \.cmd)
+  done <<< "$(ls -1 '/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/' | grep \.cmd | cut -d. -f1)"
 else
   # Create environment variable for correct distribution
   export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
