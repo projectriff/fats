8937e775576943402368090eb86916ccfb0f0ade
Scott Andrews <sandrews@pivotal.io>
2019-04-11 14:25:12 -0400

    Map all windows gcloudsdk commands into the path

--- tools/gcloud.sh
+++ tools/gcloud.sh
@@ -3,21 +3,21 @@
 if hash choco 2>/dev/null; then
   choco install gcloudsdk
 
+  # expose gcloud to the path
   cat <<EOF > /usr/bin/gcloud
 #!/bin/bash
 
 "/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/gcloud" \$@
 EOF
-  cat <<EOF > /usr/bin/gsutil
+
+  # expose all gcloudsdk *.cmd into the path
+  while read cmd; do
+    cat <<EOF > /usr/bin/$(echo ${cmd} | cut -d. -f1)
 #!/bin/bash
 
-"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/gsutil" \$@
+"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/${cmd}" \$@
 EOF
-  cat <<EOF > /usr/bin/docker-credential-gcloud
-#!/bin/bash
-
-"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/docker-credential-gcloud" \$@
-EOF
+  done < $(ls -l "/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/" | grep \.cmd)
 else
   # Create environment variable for correct distribution
   export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
