12cfb10273c28fda83e54e6e778f13382eaca6e4
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-04-15 11:49:07 -0700

    incorporate feedback

--- registries/docker-daemon/configure.sh
+++ registries/docker-daemon/configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-IMAGE_REPOSITORY_HOST="registry.riff.svc.cluster.local"
-IMAGE_REPOSITORY_PREFIX="$IMAGE_REPOSITORY_HOST:5000/u"
+IMAGE_REPOSITORY_PREFIX="registry.kube-system.svc.cluster.local:5000"
 NAMESPACE_INIT_FLAGS="${NAMESPACE_INIT_FLAGS:-} --no-secret"
 
 fats_image_repo() {
--- registries/docker-daemon/start.sh
+++ registries/docker-daemon/start.sh
@@ -6,16 +6,16 @@ docker run -d -p 5000:5000 registry:2
 
 dev_ip=172.16.1.1
 sudo su -c "echo \"\" >> /etc/hosts"
-sudo su -c "echo \"$dev_ip       $IMAGE_REPOSITORY_HOST\" >> /etc/hosts"
+sudo su -c "echo \"$dev_ip       registry.kube-system.svc.cluster.local\" >> /etc/hosts"
 sudo ifconfig lo:0 $dev_ip
-kubectl create namespace riff
+kubectl create namespace kube-system
 cat <<EOF | kubectl create -f -
 ---
 kind: Service
 apiVersion: v1
 metadata:
     name: registry
-    namespace: riff
+    namespace: kube-system
 spec:
     ports:
     - protocol: TCP
@@ -26,7 +26,7 @@ kind: Endpoints
 apiVersion: v1
 metadata:
     name: registry
-    namespace: riff
+    namespace: kube-system
 subsets:
     - addresses:
         - ip: $dev_ip
