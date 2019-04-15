78a45c2704328253d6b6bc1118a14aca64696d9c
Scott Andrews <sandrews@pivotal.io>
2019-04-15 10:44:08 -0400

    Test PKS on GCP via AZP
    Secrets are handeld as Secure Files because the value is too large to be
    saved as an environment variable.

--- clusters/pks-gcp/configure.sh
+++ clusters/pks-gcp/configure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z ${TOOLSMITH_ENV+x} ]; then
+  # load Azure Pipelines secure file
+  echo "Creating TOOLSMITH_ENV from ${DOWNLOADSECUREFILE_SECUREFILEPATH}"
+  export TOOLSMITH_ENV=$(cat "${DOWNLOADSECUREFILE_SECUREFILEPATH}" | openssl base64 -a -A)
+fi
+
 # Install pks cli
 `dirname "${BASH_SOURCE[0]}"`/../../install.sh pks
 
