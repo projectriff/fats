a224572f2f9685de7d7312f4d6c7e2892699fdd5
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-05-01 22:48:27 -0700

    make configure idempotent (#146)
    check if the file exists and contains insecure-registry url before
    restarting the docker daemon.

--- registries/docker-daemon/configure.sh
+++ registries/docker-daemon/configure.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Allow for insecure registries
-sudo su -c "echo '{ \"insecure-registries\" : [ \"registry.kube-system.svc.cluster.local\" ] }' > /etc/docker/daemon.json"
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+file='/etc/docker/daemon.json'
+if test -f ${file} && grep -q registry.kube-system ${file}; then
+  echo "insecure registry previously configured"
+else
+  # Allow for insecure registries
+  sudo su -c "echo '{ \"insecure-registries\" : [ \"registry.kube-system.svc.cluster.local\" ] }' > /etc/docker/daemon.json"
+  sudo systemctl daemon-reload
+  sudo systemctl restart docker
+fi
 
 IMAGE_REPOSITORY_PREFIX="registry.kube-system.svc.cluster.local"
 NAMESPACE_INIT_FLAGS="${NAMESPACE_INIT_FLAGS:-} --no-secret"
