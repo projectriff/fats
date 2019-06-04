4852a8c2ed60bbcc28a767d030a6c31369322383
Scott Andrews <sandrews@pivotal.io>
2019-06-04 15:40:19 -0400

    Follow minikube vmdriver none readme

--- clusters/minikube/start.sh
+++ clusters/minikube/start.sh
@@ -3,21 +3,19 @@
 if [ "$machine" == "MinGw" ]; then
   vm_driver=hyperv
 else
-  export CHANGE_MINIKUBE_NONE_USER=true
-
-  # Make root mounted as rshared to fix kube-dns issues.
-  # sudo mount --make-rshared /
-
   vm_driver=none
+
+  export MINIKUBE_HOME=$HOME
+  export CHANGE_MINIKUBE_NONE_USER=true
+  export KUBECONFIG=$HOME/.kube/config
+
+  mkdir -p $HOME/.kube $HOME/.minikube
+  touch $KUBECONFIG
 fi
 
-sudo minikube start --memory=8192 --cpus=4 \
+sudo -E minikube start --memory=8192 --cpus=4 \
   --kubernetes-version=v1.12.3 \
   --vm-driver=${vm_driver} \
   --bootstrapper=kubeadm \
   --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook" \
   --insecure-registry registry.kube-system.svc.cluster.local
-
-# Fix permissions issue in AzurePipelines
-sudo chmod --recursive 777 $HOME/.minikube
-sudo chmod --recursive 777 $HOME/.kube
