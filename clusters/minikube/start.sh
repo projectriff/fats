65864432a6d8647b611956d975c91e9b17ee3b1d
Scott Andrews <sandrews@pivotal.io>
2019-06-04 15:20:55 -0400

    Upgrade minikube to v1.1

--- clusters/minikube/start.sh
+++ clusters/minikube/start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# inspired by https://github.com/LiliC/travis-minikube/blob/minikube-30-kube-1.12/.travis.yml
-
 if [ "$machine" == "MinGw" ]; then
   vm_driver=hyperv
 else
   export CHANGE_MINIKUBE_NONE_USER=true
 
   # Make root mounted as rshared to fix kube-dns issues.
-  sudo mount --make-rshared /
+  # sudo mount --make-rshared /
 
   vm_driver=none
 fi
@@ -21,16 +19,5 @@ sudo minikube start --memory=8192 --cpus=4 \
   --insecure-registry registry.kube-system.svc.cluster.local
 
 # Fix permissions issue in AzurePipelines
-sudo chmod --recursive 777 $HOME/.minikube
-sudo chmod --recursive 777 $HOME/.kube
-
-# Fix the kubectl context, as it's often stale.
-sudo minikube update-context
-
-# Wait for Kubernetes to be up and ready.
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
-  until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
-
-wait_pod_selector_ready component=kube-apiserver kube-system
-wait_pod_selector_ready component=kube-scheduler kube-system
-wait_pod_selector_ready component=etcd kube-system
+# sudo chmod --recursive 777 $HOME/.minikube
+# sudo chmod --recursive 777 $HOME/.kube
