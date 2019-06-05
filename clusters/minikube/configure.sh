0831f098d7663188adea906f602754916f4f7e4d
Scott Andrews <sandrews@pivotal.io>
2019-06-04 20:32:25 -0400

    Upgrade minikube to v1.1 (#154)
    https://github.com/kubernetes/minikube/blob/v1.1.0/docs/vmdriver-none.md

--- clusters/minikube/configure.sh
+++ clusters/minikube/configure.sh
@@ -4,6 +4,10 @@
 `dirname "${BASH_SOURCE[0]}"`/../../install.sh minikube
 minikube config set embed-certs true
 
+export MINIKUBE_WANTUPDATENOTIFICATION=false
+export MINIKUBE_WANTREPORTERRORPROMPT=false
+export MINIKUBE_HOME=$HOME
+
 wait_for_ingress_ready() {
   local name=$1
   local namespace=$2
--- clusters/minikube/start.sh
+++ clusters/minikube/start.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
-# inspired by https://github.com/LiliC/travis-minikube/blob/minikube-30-kube-1.12/.travis.yml
-
 if [ "$machine" == "MinGw" ]; then
   vm_driver=hyperv
 else
-  export CHANGE_MINIKUBE_NONE_USER=true
-
-  # Make root mounted as rshared to fix kube-dns issues.
-  sudo mount --make-rshared /
-
   vm_driver=none
+
+  export MINIKUBE_HOME=$HOME
+  export CHANGE_MINIKUBE_NONE_USER=true
+  export KUBECONFIG=$HOME/.kube/config
+
+  mkdir -p $HOME/.kube $HOME/.minikube
+  touch $KUBECONFIG
 fi
 
-sudo minikube start --memory=8192 --cpus=4 \
+sudo -E minikube start --memory=8192 --cpus=4 \
   --kubernetes-version=v1.12.3 \
   --vm-driver=${vm_driver} \
   --bootstrapper=kubeadm \
   --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook" \
   --insecure-registry registry.kube-system.svc.cluster.local
-
-# Fix permissions issue in AzurePipelines
-sudo chmod --recursive 777 $HOME/.minikube
-sudo chmod --recursive 777 $HOME/.kube
-
-# Fix the kubectl context, as it's often stale.
-sudo minikube update-context
-
-# Wait for Kubernetes to be up and ready.
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
-  until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
-
-wait_pod_selector_ready component=kube-apiserver kube-system
-wait_pod_selector_ready component=kube-scheduler kube-system
-wait_pod_selector_ready component=etcd kube-system
