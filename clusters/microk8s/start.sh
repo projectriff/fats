3fa2aefd45c3e6e15ee63cfac1c1bf962e395c41
Scott Andrews <sandrews@pivotal.io>
2019-06-19 14:23:58 -0400

    remove alias of kubectl and docker

--- clusters/microk8s/start.sh
+++ clusters/microk8s/start.sh
@@ -12,9 +12,5 @@ sleep 2
 echo "wait for microk8s"
 microk8s.status --wait-ready
 
-echo "alias kubectl docker"
-sudo snap alias microk8s.kubectl kubectl
-sudo snap alias microk8s.docker docker
-
 echo "expose kube config"
 microk8s.kubectl config view --raw > $HOME/.kube/config
