06ef81617f5c236fd892f4ede34279c96454586a
Scott Andrews <sandrews@pivotal.io>
2019-06-19 14:32:07 -0400

    split enablement of dns and storage plugins

--- clusters/microk8s/start.sh
+++ clusters/microk8s/start.sh
@@ -4,12 +4,16 @@ sudo chown 0:0 /
 
 echo "install microk8s"
 sudo snap install microk8s --classic --channel=1.14/stable
-echo "wait for microk8s"
 microk8s.status --wait-ready
-echo "enable dns storage"
-microk8s.enable dns storage
+
+echo "enable dns"
+microk8s.enable dns
 sleep 2
-echo "wait for microk8s"
+microk8s.status --wait-ready
+
+echo "enable storage"
+microk8s.enable storage
+sleep 2
 microk8s.status --wait-ready
 
 echo "expose kube config"
