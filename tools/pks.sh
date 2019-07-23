a69daa8f2ebfaf3a90171c64e06eeba842bc9cc2
Scott Andrews <sandrews@pivotal.io>
2019-07-23 15:37:08 -0400

    Use the latest pivnet and pks clients (#171)

--- tools/pks.sh
+++ tools/pks.sh
@@ -4,7 +4,7 @@
 `dirname "${BASH_SOURCE[0]}"`/../install.sh pivnet
 
 # Install pks cli
-pivnet download-product-files -p pivotal-container-service -r 1.2.3 -i 268848 --accept-eula
+pivnet download-product-files -p pivotal-container-service -r 1.4.1 -i 400601 --accept-eula
 mv pks-* pks
 chmod +x pks
 sudo mv pks /usr/local/bin/
