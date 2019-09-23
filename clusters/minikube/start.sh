5d72d692fe65fdbbd6dac44703ad04ad90ee896f
Scott Andrews <sandrews@pivotal.io>
2019-09-22 20:50:27 -0400

    mirror known good config
    from https://github.com/LiliC/travis-minikube/blob/63e39ca1e25a4cc6727ca21ec03474f2f9b26980/.travis.yml

--- clusters/minikube/start.sh
+++ clusters/minikube/start.sh
@@ -3,12 +3,12 @@
 if [ "$machine" == "MinGw" ]; then
   vm_driver=hyperv
 else
-  sudo `dirname "${BASH_SOURCE[0]}"`/install-docker.sh
-
   vm_driver=none
 
-  export MINIKUBE_HOME=$HOME
   export CHANGE_MINIKUBE_NONE_USER=true
+  export MINIKUBE_WANTUPDATENOTIFICATION=false
+  export MINIKUBE_WANTREPORTERRORPROMPT=false
+  export MINIKUBE_HOME=$HOME
   export KUBECONFIG=$HOME/.kube/config
 
   mkdir -p $HOME/.kube $HOME/.minikube
@@ -18,6 +18,7 @@ fi
 sudo -E minikube start --memory=8192 --cpus=4 \
   --kubernetes-version=v1.14.7 \
   --vm-driver=${vm_driver} \
-  --bootstrapper=kubeadm \
-  --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook" \
   --insecure-registry registry.kube-system.svc.cluster.local
+
+me=$(whoami)
+sudo chown -R ${me}: $HOME/.minikube/
