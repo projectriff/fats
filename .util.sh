1a1cba59e0ae8c959d525717489a5dd302b39393
Scott Andrews <sandrews@pivotal.io>
2019-10-01 11:54:29 -0400

    Add retry for no resource request wehbook
    Sometimes the cert-manager webhook just isn't ready, despite the backing
    pods being ready

--- .util.sh
+++ .util.sh
@@ -125,3 +125,22 @@ wait_kube_ready() {
 fats_echo() {
   echo -e "$ANSI_BLUE[`date -u +%Y-%m-%dT%H:%M:%SZ`]$ANSI_RESET $@"
 }
+
+fats_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+    }
+    # ! { } ignores set -e, see https://stackoverflow.com/a/4073372
+    ! { "$@"; result=$?; }
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+  done
+    [ $count -gt 3 ] && {
+      echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+    }
+  return $result
+}
