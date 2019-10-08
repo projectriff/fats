276f456e38f0ae62d1a0fb0ea55a60c655ea1566
Scott Andrews <sandrews@pivotal.io>
2019-10-08 12:34:15 -0400

    Reintroduce basic log folding
    FATS previously had rich log folding when running on Travis, but AZP
    didn't support the capability, so we removed the support.
    
    Since GitHub Actions supports a single level of log folding, we can
    reintroduce basic support.

--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@
 
 source `dirname "${BASH_SOURCE[0]}"`/.configure.sh
 
-echo "Starting cluster $CLUSTER"
+echo "##[group]Starting cluster $CLUSTER"
 source `dirname "${BASH_SOURCE[0]}"`/clusters/${CLUSTER}/start.sh
+echo "##[endgroup]"
 
-echo "Starting registry $REGISTRY"
+echo "##[group]Starting registry $REGISTRY"
 source `dirname "${BASH_SOURCE[0]}"`/registries/${REGISTRY}/start.sh
-
 post_registry_start $REGISTRY
+echo "##[endgroup]"
