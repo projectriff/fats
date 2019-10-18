5a16eda06d460a9c4abb1ab9d50740a52fb7bd71
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-18 11:25:23 -0700

    install yq

--- macros/helm-hack.sh
+++ macros/helm-hack.sh
@@ -6,5 +6,6 @@ cd ..
 git clone https://github.com/projectriff/charts.git
 cd charts
 echo "keda: https://raw.githubusercontent.com/sbawaska/keda/rm-clusterrole/deploy/KedaScaleController.yaml" > charts/keda/templates.yaml
+sudo snap install yq
 make
 cd ../fats
