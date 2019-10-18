d49526ef93966754e863857f6171d642c0742ada
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-18 12:44:45 -0700

    fix paths

--- macros/helm-hack.sh
+++ macros/helm-hack.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -x
 # TODO remove before merging
-
-cd ..
-git clone https://github.com/projectriff/charts.git
-cd charts
+old_dir=`pwd`
+charts_dir=$fats_dir/../charts
+mkdir -p $charts_dir
+cd $charts_dir
+git clone https://github.com/projectriff/charts.git .
 echo "keda: https://raw.githubusercontent.com/sbawaska/keda/rm-clusterrole/deploy/KedaScaleController.yaml" > charts/keda/templates.yaml
 sudo snap install yq
-../fats/install.sh ytt 0.14.0
 make
-cd ../fats
+cd $old_dir
