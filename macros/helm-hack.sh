0a42a16728c83678118bb89dcb48b12e9def830f
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-18 12:26:15 -0700

    fix install path

--- macros/helm-hack.sh
+++ macros/helm-hack.sh
@@ -7,6 +7,6 @@ git clone https://github.com/projectriff/charts.git
 cd charts
 echo "keda: https://raw.githubusercontent.com/sbawaska/keda/rm-clusterrole/deploy/KedaScaleController.yaml" > charts/keda/templates.yaml
 sudo snap install yq
-../fats install.sh ytt 0.14.0
+../fats/install.sh ytt 0.14.0
 make
 cd ../fats
