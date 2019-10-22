4e05b1332288cfafe74af545395bb62334b57cf7
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-22 14:34:57 -0700

    create kafka provider using cli

--- macros/streaming-prereq-install.sh
+++ macros/streaming-prereq-install.sh
@@ -3,14 +3,6 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install --name my-kafka bitnami/kafka
 
-cat <<EOF | kubectl create -f -
----
-apiVersion: streaming.projectriff.io/v1alpha1
-kind: KafkaProvider
-metadata:
-  name: franz
-spec:
-  bootstrapServers: my-kafka:9092
-EOF
+riff streaming kafka-provider create franz --bootstrap-servers my-kafka:9092
 
 kubectl apply -f https://storage.googleapis.com/projectriff/riff-http-gateway/riff-http-gateway-0.5.0-snapshot.yaml
