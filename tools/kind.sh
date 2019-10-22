0fc2158a62075d82f878911146681302f39a38ae
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-22 14:45:43 -0700

    retry fetching kind binary
    getting too many "Could not resolve host: github-production-release-asset"

--- tools/kind.sh
+++ tools/kind.sh
@@ -2,12 +2,13 @@
 
 kind_version="${1:-v0.5.1}"
 base_url="${2:-https://github.com/kubernetes-sigs/kind/releases/download}"
+curl_retries=3
 
 if [ "$machine" == "MinGw" ]; then
-  curl -Lo kind.exe ${base_url}/${kind_version}/kind-windows-amd64
+  curl -Lo kind.exe ${base_url}/${kind_version}/kind-windows-amd64 --retry ${curl_retries}
   mv kind.exe /usr/bin/
 else
-  curl -Lo kind ${base_url}/${kind_version}/kind-linux-amd64
+  curl -Lo kind ${base_url}/${kind_version}/kind-linux-amd64 --retry ${curl_retries}
   chmod +x kind
   sudo mv kind /usr/local/bin/
 fi
