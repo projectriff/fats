e6aa40ec757d3bd538e3d44a23d320e940830f8b
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-29 16:17:15 -0700

    adding streaming java and java-boot functions

---
+++ functions/repeater/java-boot/src/test/java/io/projectriffsamples/bootrepeater/BootRepeaterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projectriffsamples.bootrepeater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRepeaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
