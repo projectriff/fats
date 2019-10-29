2101e69a0b1267ad80e912f0b1729f0db064a392
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-29 13:10:13 -0700

    port forward http gateway

--- macros/streaming-prereq-install.sh
+++ macros/streaming-prereq-install.sh
@@ -6,3 +6,6 @@ helm install --name my-kafka bitnami/kafka
 riff streaming kafka-provider create franz --bootstrap-servers my-kafka:9092
 
 kubectl apply -f https://storage.googleapis.com/projectriff/riff-http-gateway/riff-http-gateway-0.5.0-snapshot.yaml
+
+kubectl -n "riff-system" port-forward "svc/riff-streaming-http-gateway" "8080:80" &	
+pf_pid=$!
--- macros/streaming-prereq-uninstall.sh
+++ macros/streaming-prereq-uninstall.sh
@@ -5,3 +5,6 @@ helm delete --purge my-kafka
 riff streaming kafka-provider delete franz
 
 kubectl delete -f https://storage.googleapis.com/projectriff/riff-http-gateway/riff-http-gateway-0.5.0-snapshot.yaml
+
+kill $pf_pid || true
+kill $li_pid || true
