e4ba05809078ac640b3a3735688a1de3c44e6fde
Scott Andrews <sandrews@pivotal.io>
2019-11-11 11:14:56 -0500

    Skip tool install if not in CI

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ ! -f `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed ]; then
-  touch `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed
-  source `dirname "${BASH_SOURCE[0]}"`/.util.sh
-
-  echo "Installing $1"
-  source `dirname "${BASH_SOURCE[0]}"`/tools/$1.sh "${2:-}" "${3:-}"
+if [ -z "${CI:-}" ] || [ -f `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed ]; then
+  # skip if not run by CI or tool previously installed
+  exit 0
 fi
+
+touch `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed
+source `dirname "${BASH_SOURCE[0]}"`/.util.sh
+
+echo "Installing $1"
+source `dirname "${BASH_SOURCE[0]}"`/tools/$1.sh "${2:-}" "${3:-}"
