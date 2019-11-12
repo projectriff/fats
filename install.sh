bc87f1a1215efbbcb8fbcb0e5d21996dfcdf2342
Scott Andrews <sandrews@pivotal.io>
2019-11-11 19:22:59 -0500

    Skip tool install if not in CI (#232)

--- install.sh
+++ install.sh
@@ -4,10 +4,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ ! -f `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed ]; then
-  touch `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed
-  source `dirname "${BASH_SOURCE[0]}"`/.util.sh
-
-  echo "Installing $1"
-  source `dirname "${BASH_SOURCE[0]}"`/tools/$1.sh "${2:-}" "${3:-}"
+if [ -z "${CI:-}" ] && [ -z "${GITHUB_WORKSPACE:-}" ]; then
+  echo "Skipping $1 install for dev environment"
+  exit 0
 fi
+
+if [ -f `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed ]; then
+  # tool previously installed
+  exit 0
+fi
+
+touch `dirname "${BASH_SOURCE[0]}"`/tools/$1.installed
+source `dirname "${BASH_SOURCE[0]}"`/.util.sh
+
+echo "Installing $1"
+source `dirname "${BASH_SOURCE[0]}"`/tools/$1.sh "${2:-}" "${3:-}"
