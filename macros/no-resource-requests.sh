8cea655c13cd49265d2d51aa68a5a9b806656dec
Dave Syer <dsyer@pivotal.io>
2019-11-12 12:10:33 +0000

    Fix typo

--- macros/no-resource-requests.sh
+++ macros/no-resource-requests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $(kubectl get nodes -oname | wc -l) = "1" ]; then
-  echo "Elimiate pod resource requests"
+  echo "Eliminate pod resource requests"
   fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/no-resource-requests-webhook/no-resource-requests-webhook.yaml
   wait_pod_selector_ready app=webhook no-resource-requests
 fi
