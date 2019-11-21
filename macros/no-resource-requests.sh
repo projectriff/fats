6d82d7d15c4e38c24ce4b8833c3d880b9d5d36fb
Scott Andrews <sandrews@pivotal.io>
2019-11-21 18:02:54 -0500

    Update no-resource-requests-webhook for cert-manager 0.11

--- macros/no-resource-requests.sh
+++ macros/no-resource-requests.sh
@@ -2,6 +2,6 @@
 
 if [ $(kubectl get nodes -oname | wc -l) = "1" ]; then
   echo "Eliminate pod resource requests"
-  fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/no-resource-requests-webhook/no-resource-requests-webhook.yaml
+  fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/no-resource-requests-webhook/no-resource-requests-webhook-20191121210956-521ae2a8c3323540.yaml
   wait_pod_selector_ready app=webhook no-resource-requests
 fi
