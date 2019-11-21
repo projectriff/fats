39219413b9b84f9a494cc92d2afc102a774aed2c
Scott Andrews <sandrews@pivotal.io>
2019-11-21 18:30:12 -0500

    parameterize webhook version

--- macros/no-resource-requests.sh
+++ macros/no-resource-requests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+webhook_version="${1:-20191121210956-521ae2a8c3323540}"
+
 if [ $(kubectl get nodes -oname | wc -l) = "1" ]; then
   echo "Eliminate pod resource requests"
-  fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/no-resource-requests-webhook/no-resource-requests-webhook-20191121210956-521ae2a8c3323540.yaml
+  fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/no-resource-requests-webhook/no-resource-requests-webhook-${webhook_version}.yaml
   wait_pod_selector_ready app=webhook no-resource-requests
 fi
