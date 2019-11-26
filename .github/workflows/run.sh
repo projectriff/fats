d87311851ae11ff8dc03ac1291bac3cc3522344a
Scott Andrews <sandrews@pivotal.io>
2019-11-26 10:32:10 -0500

    disable non-streaming tests

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -12,73 +12,74 @@ ${FATS_DIR}/install.sh kubectl
 
 fats_create_push_credentials ${NAMESPACE}
 
-# in cluster builds
-# workaround for https://github.com/projectriff/node-function-invoker/issues/113
-if [ ${CLUSTER} = "pks-gcp" ]; then
-  languages="java java-boot command"
-else
-  languages="java java-boot node npm command"
-fi
-for test in ${languages}; do
-  name=fats-cluster-uppercase-${test}
-  image=$(fats_image_repo ${name})
+# TODO uncomment before merging
+# # in cluster builds
+# # workaround for https://github.com/projectriff/node-function-invoker/issues/113
+# if [ ${CLUSTER} = "pks-gcp" ]; then
+#   languages="java java-boot command"
+# else
+#   languages="java java-boot node npm command"
+# fi
+# for test in ${languages}; do
+#   name=fats-cluster-uppercase-${test}
+#   image=$(fats_image_repo ${name})
 
-  echo "##[group]Run function ${name}"
+#   echo "##[group]Run function ${name}"
 
-  riff function create ${name} --image ${image} --namespace ${NAMESPACE} --tail \
-    --git-repo https://github.com/${FATS_REPO} --git-revision ${FATS_REFSPEC} --sub-path functions/uppercase/${test} &
-  riff core deployer create ${name} --function-ref ${name} --namespace ${NAMESPACE} --tail
+#   riff function create ${name} --image ${image} --namespace ${NAMESPACE} --tail \
+#     --git-repo https://github.com/${FATS_REPO} --git-revision ${FATS_REFSPEC} --sub-path functions/uppercase/${test} &
+#   riff core deployer create ${name} --function-ref ${name} --namespace ${NAMESPACE} --tail
 
-  source ${FATS_DIR}/macros/invoke_core_deployer.sh ${name} "-H Content-Type:text/plain -H Accept:text/plain -d fats" FATS
+#   source ${FATS_DIR}/macros/invoke_core_deployer.sh ${name} "-H Content-Type:text/plain -H Accept:text/plain -d fats" FATS
 
-  riff core deployer delete ${name} --namespace ${NAMESPACE}
-  riff function delete ${name} --namespace ${NAMESPACE}
-  fats_delete_image ${image}
+#   riff core deployer delete ${name} --namespace ${NAMESPACE}
+#   riff function delete ${name} --namespace ${NAMESPACE}
+#   fats_delete_image ${image}
 
-  echo "##[endgroup]"
-done
+#   echo "##[endgroup]"
+# done
 
-# local builds
-if [ "${machine}" != "MinGw" ]; then
-  # TODO enable for windows once we have a linux docker daemon available
-  for test in ${languages}; do
-    name=fats-local-uppercase-${test}
-    image=$(fats_image_repo ${name})
+# # local builds
+# if [ "${machine}" != "MinGw" ]; then
+#   # TODO enable for windows once we have a linux docker daemon available
+#   for test in ${languages}; do
+#     name=fats-local-uppercase-${test}
+#     image=$(fats_image_repo ${name})
 
-    echo "##[group]Run function ${name}"
+#     echo "##[group]Run function ${name}"
 
-    riff function create ${name} --image ${image} --namespace ${NAMESPACE} --tail \
-      --local-path ${FATS_DIR}/functions/uppercase/${test} &
-    riff knative deployer create ${name} --function-ref ${name} --namespace ${NAMESPACE} --tail
+#     riff function create ${name} --image ${image} --namespace ${NAMESPACE} --tail \
+#       --local-path ${FATS_DIR}/functions/uppercase/${test} &
+#     riff knative deployer create ${name} --function-ref ${name} --namespace ${NAMESPACE} --tail
 
-    source ${FATS_DIR}/macros/invoke_knative_deployer.sh ${name} "-H Content-Type:text/plain -H Accept:text/plain -d fats" FATS
+#     source ${FATS_DIR}/macros/invoke_knative_deployer.sh ${name} "-H Content-Type:text/plain -H Accept:text/plain -d fats" FATS
 
-    riff knative deployer delete ${name} --namespace ${NAMESPACE}
-    riff function delete ${name} --namespace ${NAMESPACE}
-    fats_delete_image ${image}
+#     riff knative deployer delete ${name} --namespace ${NAMESPACE}
+#     riff function delete ${name} --namespace ${NAMESPACE}
+#     fats_delete_image ${image}
 
-    echo "##[endgroup]"
-  done
-fi
+#     echo "##[endgroup]"
+#   done
+# fi
 
-for test in java-boot node; do
-  name=fats-application-uppercase-${test}
-  image=$(fats_image_repo ${name})
+# for test in java-boot node; do
+#   name=fats-application-uppercase-${test}
+#   image=$(fats_image_repo ${name})
 
-  echo "##[group]Run application ${name}"
+#   echo "##[group]Run application ${name}"
 
-  riff application create ${name} --image ${image} --namespace ${NAMESPACE} --tail \
-    --git-repo https://github.com/${FATS_REPO} --git-revision ${FATS_REFSPEC} --sub-path applications/uppercase/${test} &
-  riff core deployer create ${name} --application-ref ${name} --namespace ${NAMESPACE} --tail
+#   riff application create ${name} --image ${image} --namespace ${NAMESPACE} --tail \
+#     --git-repo https://github.com/${FATS_REPO} --git-revision ${FATS_REFSPEC} --sub-path applications/uppercase/${test} &
+#   riff core deployer create ${name} --application-ref ${name} --namespace ${NAMESPACE} --tail
 
-  source ${FATS_DIR}/macros/invoke_core_deployer.sh ${name} "--get --data-urlencode input=fats" FATS
+#   source ${FATS_DIR}/macros/invoke_core_deployer.sh ${name} "--get --data-urlencode input=fats" FATS
 
-  riff core deployer delete ${name} --namespace ${NAMESPACE}
-  riff application delete ${name} --namespace ${NAMESPACE}
-  fats_delete_image ${image}
+#   riff core deployer delete ${name} --namespace ${NAMESPACE}
+#   riff application delete ${name} --namespace ${NAMESPACE}
+#   fats_delete_image ${image}
 
-  echo "##[endgroup]"
-done
+#   echo "##[endgroup]"
+# done
 
 # test streaming
 riff streaming kafka-provider create franz --bootstrap-servers my-kafka:9092 --namespace $NAMESPACE
