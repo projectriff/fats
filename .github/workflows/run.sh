60b9a4cd7df405a44a47b2551ad7e989feaa93ad
Scott Andrews <sandrews@pivotal.io>
2019-11-26 16:39:31 -0500

    Skip processor, echo from same stream

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -15,6 +15,19 @@ fats_create_push_credentials ${NAMESPACE}
 source ${FATS_DIR}/macros/install-dev-utils.sh
 riff streaming kafka-provider create franz --bootstrap-servers kafka.kafka.svc.cluster.local:9092 --namespace $NAMESPACE
 
+# streaming runtime (debug)
+riff streaming stream create echo --namespace $NAMESPACE --provider franz-kafka-provisioner --content-type 'text/plain'
+kubectl wait streams.streaming.projectriff.io echo --for=condition=Ready --namespace $NAMESPACE --timeout=60s
+kubectl exec dev-utils -n $NAMESPACE -- subscribe echo -n $NAMESPACE --payload-as-string | tee result.txt &
+subscribe_exec=$?
+sleep 5
+kubectl exec dev-utils -n $NAMESPACE -- publish echo -n $NAMESPACE --payload "fats" --content-type "text/plain"
+verify_payload result.txt "fats"
+kill $subscribe_exec
+riff streaming stream delete echo --namespace $NAMESPACE
+
+
+
 if [ "${machine}" != "MinGw" ]; then
   modes="cluster local"
 else
