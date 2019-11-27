ec18cd728b431b2c44e1e8db6eb99537bf6dc6ff
Scott Andrews <sandrews@pivotal.io>
2019-11-26 21:17:52 -0500

    drop install kafka macro

--- macros/install-kafka.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# install kafka for streaming
-helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
-helm install --name kafka incubator/kafka --set replicas=1,zookeeper.replicaCount=1,zookeeper.env.ZK_HEAP_SIZE=128m --namespace kafka --wait
