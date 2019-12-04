2f85871ce75cd7b6c91c4ca4d1be612ad35582d9
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-12-04 22:54:04 +0530

    show kafka topics

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -95,6 +95,24 @@ for mode in ${modes}; do
         sleep 1
       done
 
+      cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: testclient
+  namespace: kafka
+spec:
+  containers:
+  - name: kafka
+    image: confluentinc/cp-kafka:5.0.1
+    command:
+      - sh
+      - -c
+      - "exec tail -f /dev/null"
+EOF
+      kubectl -n kafka exec -ti testclient -- kafka-console-consumer --bootstrap-server kafka.kafka.svc.cluster.local:9092 --topic test1_fats-cluster-fn-uppercase-java-lower --from-beginning --timeout-ms 10000
+      kubectl -n kafka exec -ti testclient -- kafka-console-consumer --bootstrap-server kafka.kafka.svc.cluster.local:9092 --topic test1_fats-cluster-fn-uppercase-java-upper --from-beginning --timeout-ms 10000
+
       fats_assert "$expected_data" "$actual_data"
 
       kubectl exec riff-dev -n $NAMESPACE -- sh -c 'kill $(pidof subscribe)'
