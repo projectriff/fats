8ad19918903f6009d77c3d26f27c47cc398cf2b5
Scott Andrews <sandrews@pivotal.io>
2019-12-23 11:48:17 -0500

    normalize path

--- tools/gcloud.sh
+++ tools/gcloud.sh
@@ -2,23 +2,14 @@
 
 if ! [ -x "$(command -v gcloud)" ]; then
   if hash choco 2>/dev/null; then
+    env
+
     choco install gcloudsdk --ignore-checksums || true
 
-    # expose gcloud to the path
-    cat <<EOF > /usr/bin/gcloud
-#!/bin/bash
+    # add to current and future PATH
+    export PATH="${PATH}:/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin"
+    echo "##[add-path]/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin"
 
-"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/gcloud" \$@
-EOF
-
-    # expose all gcloudsdk *.cmd into the path
-    while read -r cmd; do
-      cat <<EOF > /usr/bin/${cmd}
-#!/bin/bash
-
-"/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/${cmd}.cmd" \$@
-EOF
-    done <<< "$(ls -1 '/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/' | grep \.cmd | cut -d. -f1)"
   else
     # Create environment variable for correct distribution
     export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
