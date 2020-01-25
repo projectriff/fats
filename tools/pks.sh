ff8866924b5ac8e33fca6b87275ee93d843488bf
Scott Andrews <sandrews@pivotal.io>
2020-01-25 14:13:21 -0500

    Upgrade PKS to 1.6, k8s 1.15

--- tools/pks.sh
+++ tools/pks.sh
@@ -4,7 +4,7 @@
 `dirname "${BASH_SOURCE[0]}"`/../install.sh pivnet
 
 # Install pks cli
-pivnet download-product-files --product-slug='pivotal-container-service' --release-version='1.5.1' --product-file-id=496006 --accept-eula
+pivnet download-product-files --product-slug='pivotal-container-service' --release-version='1.6.1' --product-file-id=579531 --accept-eula
 mv pks-* pks
 chmod +x pks
 sudo mv pks /usr/local/bin/
