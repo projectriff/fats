81a208ba58317171dceb2a82824f888dc728dc7f
Scott Andrews <sandrews@pivotal.io>
2020-01-28 16:32:09 -0500

    Bump dev-utils (#259)

--- macros/create-riff-dev-pod.sh
+++ macros/create-riff-dev-pod.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-utils_version=0.5.0-snapshot-20200116110522-3c8772342bd5cc9f
+utils_version=0.5.0-snapshot-20200128184245-26294c4d71be20f1
 
-kubectl create serviceaccount riff-dev --namespace $NAMESPACE
-kubectl create role view-secrets --namespace $NAMESPACE --resource secrets --verb get,watch,list
-kubectl create rolebinding riff-dev-view --namespace $NAMESPACE --clusterrole=view --serviceaccount=${NAMESPACE}:riff-dev
-kubectl create rolebinding riff-dev-view-secrets --namespace $NAMESPACE --role=view-secrets --serviceaccount=${NAMESPACE}:riff-dev
-
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: riff-dev
-  namespace: ${NAMESPACE}
-spec:
-  serviceAccountName: riff-dev
-  containers:
-  - name: utils
-    image: projectriff/dev-utils:${utils_version}
-EOF
-kubectl wait pods --for=condition=Ready riff-dev --namespace $NAMESPACE --timeout=60s
-
+kubectl create serviceaccount riff-dev --namespace=${NAMESPACE}
+kubectl create rolebinding riff-dev-edit --namespace=${NAMESPACE} --clusterrole=edit --serviceaccount=${NAMESPACE}:riff-dev
+kubectl run riff-dev --namespace=${NAMESPACE} --image=projectriff/dev-utils --serviceaccount=riff-dev --generator=run-pod/v1
+kubectl wait pods riff-dev --for=condition=Ready --namespace=$NAMESPACE --timeout=60s
