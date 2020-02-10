8a753bd3b1bb1bf4d8a7ad9c0dba730a5d228d50
Scott Andrews <sandrews@pivotal.io>
2020-02-10 13:14:38 -0500

    expose dns inside cluster, define as insecure registry

--- clusters/kind/start.sh
+++ clusters/kind/start.sh
@@ -6,12 +6,13 @@ apiVersion: kind.x-k8s.io/v1alpha4
 EOF
 
 if [ "$REGISTRY" = "docker-daemon" ] ; then
+  registry_ip=$(docker inspect --format "{{.NetworkSettings.IPAddress }}" registry)
   # patch cluster config for registry location
   cat <<EOF >> ${CLUSTER_NAME}.yaml
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry.kube-system.svc.cluster.local:5000"]
-    endpoint = ["http://$(docker inspect --format "{{.NetworkSettings.IPAddress }}" registry):5000"]
+    endpoint = ["http://${registry_ip}:5000"]
 EOF
 fi
 
@@ -21,7 +22,33 @@ kind create cluster --name ${CLUSTER_NAME} \
   --wait 5m
 
 if [ "$REGISTRY" = "docker-daemon" ] ; then
-  docker exec ${CLUSTER_NAME}-control-plane bash -c "echo \"$(docker inspect --format "{{.NetworkSettings.IPAddress }}" registry) registry.kube-system.svc.cluster.local\" >> /etc/hosts"
+  docker exec ${CLUSTER_NAME}-control-plane bash -c "echo \"${registry_ip} registry.kube-system.svc.cluster.local\" >> /etc/hosts"
+  sudo su -c "echo \"${registry_ip} registry.kube-system.svc.cluster.local\" >> /etc/hosts"
+
+  cat <<EOF | kubectl create -f -
+---
+kind: Service
+apiVersion: v1
+metadata:
+  name: registry
+  namespace: kube-system
+spec:
+  ports:
+  - protocol: TCP
+    port: 5000
+    targetPort: 5000
+---
+kind: Endpoints
+apiVersion: v1
+metadata:
+  name: registry
+  namespace: kube-system
+subsets:
+  - addresses:
+    - ip: ${registry_ip}
+    ports:
+      - port: 5000
+EOF
 fi
 
 # move kubeconfig to expected location
--- registries/docker-daemon/configure.sh
+++ registries/docker-daemon/configure.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Allow for insecure registries as long as docker daemon is actually running
-if ! grep -q docker /proc/1/cgroup; then
-  daemonConfig='/etc/docker/daemon.json'
-  if test -f ${daemonConfig} && grep -q registry.kube-system.svc.cluster.local ${daemonConfig}; then
-    echo "insecure registry previously configured"
-  else
-    echo '{ "insecure-registries": [ "registry.kube-system.svc.cluster.local:5000" ] }' | sudo tee ${daemonConfig} > /dev/null
-    sudo systemctl daemon-reload
-    sudo systemctl restart docker
-  fi
-else
-  # test that registry is configured correctly
-  docker pull cloudfoundry/run:tiny \
-    && docker tag cloudfoundry/run:tiny registry.kube-system.svc.cluster.local:5000/run:tiny \
-    && docker push registry.kube-system.svc.cluster.local:5000/run:tiny
-fi
-
 IMAGE_REPOSITORY_PREFIX="registry.kube-system.svc.cluster.local:5000"
 
 fats_image_repo() {
