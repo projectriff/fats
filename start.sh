9ae0a6b7e2c046420bf7978275a9a8ae9e8f5527
Scott Andrews <sandrews@pivotal.io>
2020-02-10 11:18:15 -0500

    Invert cluster and registry relationship

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 source `dirname "${BASH_SOURCE[0]}"`/.configure.sh
 
-echo "##[group]Starting cluster $CLUSTER"
-source `dirname "${BASH_SOURCE[0]}"`/clusters/${CLUSTER}/start.sh
-echo "##[endgroup]"
-
 echo "##[group]Starting registry $REGISTRY"
 source `dirname "${BASH_SOURCE[0]}"`/registries/${REGISTRY}/start.sh
 post_registry_start $REGISTRY
+echo "##[endgroup]"
+
+echo "##[group]Starting cluster $CLUSTER"
+source `dirname "${BASH_SOURCE[0]}"`/clusters/${CLUSTER}/start.sh
 echo "##[endgroup]"
