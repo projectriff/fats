a267e70c62007bb4e7848bfa0af74adec44bda76
Swapnil Bawaskar <sbawaskar@pivotal.io>
2020-08-20 06:59:28 -0700

    install gcloud from versioned archive

--- tools/gcloud.sh
+++ tools/gcloud.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+gcloud_dir="${1:-google-cloud-sdk}"
+mkdir -p $gcloud_dir
+
 if ! [ -x "$(command -v gcloud)" ]; then
   if hash choco 2>/dev/null; then
     choco install gcloudsdk --ignore-checksums
@@ -20,17 +23,10 @@ EOF
 EOF
     done <<< "$(ls -1 '/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin/' | grep \.cmd | cut -d. -f1)"
   else
-    # Create environment variable for correct distribution
-    export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-
-    # Add the Cloud SDK distribution URI as a package source
-    echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
-    # Import the Google Cloud Platform public key
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-    # Update the package list and install the Cloud SDK
-    sudo apt-get update && sudo apt-get install google-cloud-sdk
+    # install a versioned archive
+    curl -L https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-306.0.0-linux-x86_64.tar.gz | tar xz -C $gcloud_dir --strip-components 1
+    ./$gcloud_dir/install.sh --quiet
+    source $gcloud_dir/path.bash.inc
   fi
 fi
 
